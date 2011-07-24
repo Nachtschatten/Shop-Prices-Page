127996f9425d656a62ddd572736f7fd8358edf28
Nachtschatten <vance800-github@yahoo.es>
2011-07-24 23:12:46 +0200

    First try at a more useful sorting than simply by ID.

--- priceInfo.coffee
+++ priceInfo.coffee
@@ -8,8 +8,32 @@ generatePriceInfoDiv = (item) ->
 
 	return $('<div class=product>' + priceLDiv + iconDiv + priceRDiv + '</div>');
 
+
+compare = (item1, item2) ->
+	matDifference = getMaterialValue(item1.name) - getMaterialValue(item2.name)
+	if matDifference is 0
+		return item1.id - item2.id
+	return matDifference
+
+getMaterialValue = (name) ->
+	name = name.toLowerCase()
+	if name.indexOf("wood") isnt -1
+		return 1
+	if name.indexOf("stone") isnt -1 and name.indexOf("redstone") is -1
+		return 2
+	if name.indexOf("rack") isnt -1
+		return 2
+	if name.indexOf("iron") isnt -1
+		return 3
+	if name.indexOf("diamond") isnt -1
+		return 4
+	if name.indexOf("gold") isnt -1
+		return 5
+	return 0
+
 $.getJSON 'price_json.php', (data) ->
 	for type, items of data
+		items.sort(compare)
 		for item in items
 			$("##{type}").append generatePriceInfoDiv item
 	$(document).trigger 'itemsloaded'
--- priceInfo.js
+++ priceInfo.js
@@ -1,5 +1,5 @@
 (function() {
-  var generatePriceInfoDiv;
+  var compare, generatePriceInfoDiv, getMaterialValue;
   generatePriceInfoDiv = function(item) {
     var iconDiv, price, priceLDiv, priceRDiv;
     price = function(c, p, p64) {
@@ -10,10 +10,42 @@
     priceRDiv = price('priceR', item.sell1, item.sell64);
     return $('<div class=product>' + priceLDiv + iconDiv + priceRDiv + '</div>');
   };
+  compare = function(item1, item2) {
+    var matDifference;
+    matDifference = getMaterialValue(item1.name) - getMaterialValue(item2.name);
+    if (matDifference === 0) {
+      return item1.id - item2.id;
+    }
+    return matDifference;
+  };
+  getMaterialValue = function(name) {
+    name = name.toLowerCase();
+    if (name.indexOf("wood") !== -1) {
+      return 1;
+    }
+    if (name.indexOf("stone") !== -1 && name.indexOf("redstone") === -1) {
+      return 2;
+    }
+    if (name.indexOf("rack") !== -1) {
+      return 2;
+    }
+    if (name.indexOf("iron") !== -1) {
+      return 3;
+    }
+    if (name.indexOf("diamond") !== -1) {
+      return 4;
+    }
+    if (name.indexOf("gold") !== -1) {
+      return 5;
+    }
+    return 0;
+  };
   $.getJSON('price_json.php', function(data) {
     var item, items, type, _i, _len;
     for (type in data) {
       items = data[type];
+      items.sort(compare);
+      alert(type);
       for (_i = 0, _len = items.length; _i < _len; _i++) {
         item = items[_i];
         $("#" + type).append(generatePriceInfoDiv(item));
