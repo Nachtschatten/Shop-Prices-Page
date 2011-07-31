68858a6b8a2866108f522b9d2935410b9af8e54f
Luchs <Luchs@euirc.eu>
2011-08-01 00:33:01 +0200

    First pages commit

---
+++ priceInfo.js
@@ -0,0 +1,152 @@
+(function() {
+  var compare, generatePriceInfoDiv, getMaterialValue, setViewport;
+  generatePriceInfoDiv = function(item) {
+    var iconDiv, price, priceLDiv, priceRDiv;
+    price = function(c, p, p64) {
+      return "<div class=" + c + ">" + p + "<br><span>" + p64 + "</span></div>";
+    };
+    priceLDiv = price('priceL', item.buy1, item.buy64);
+    if (item.name === "Yellow flower") {
+      item.picurl = "http://www.minecraftwiki.net/images/4/49/Grid_Dandelion.png";
+    }
+    iconDiv = "<div class=icon><img src='" + item.picurl + "' alt='" + item.name + "' title='" + item.name + "'></div>";
+    priceRDiv = price('priceR', item.sell1, item.sell64);
+    return $('<div class=product>' + priceLDiv + iconDiv + priceRDiv + '</div>');
+  };
+  compare = function(items, item1, item2) {
+    var matDifference;
+    matDifference = getMaterialValue(item1.name) - getMaterialValue(item2.name);
+    if (matDifference === 0) {
+      return items.indexOf(item1) - items.indexOf(item2);
+    }
+    return matDifference;
+  };
+  getMaterialValue = function(name) {
+    var nameContains;
+    name = name.toLowerCase();
+    nameContains = function() {
+      var word, _i, _len;
+      for (_i = 0, _len = arguments.length; _i < _len; _i++) {
+        word = arguments[_i];
+        if (name.indexOf(word) !== -1) {
+          return true;
+        }
+      }
+      return false;
+    };
+    if (nameContains('sapling')) {
+      return 1;
+    }
+    if (nameContains('leaves', 'birch tree', 'redwood tree')) {
+      return 2;
+    }
+    if (nameContains('workbench', 'furnace', 'chest', 'dispenser' && !nameContains('plate'))) {
+      return 3;
+    }
+    if (nameContains('jukebox', 'note block')) {
+      return 4;
+    }
+    if (nameContains('rail')) {
+      return 5;
+    }
+    if (nameContains('bucket')) {
+      return 6;
+    }
+    if (nameContains('music disc')) {
+      return 7;
+    }
+    if (nameContains('minecart')) {
+      return 8;
+    }
+    if (nameContains('leather')) {
+      return 15;
+    }
+    if (nameContains('wood' || name === 'birch')) {
+      return 20;
+    }
+    if (nameContains('sand')) {
+      return 25;
+    }
+    if (nameContains('stone' && !nameContains('redstone'))) {
+      return 30;
+    }
+    if (nameContains('rack')) {
+      return 30;
+    }
+    if (nameContains('iron')) {
+      return 35;
+    }
+    if (nameContains('diamond')) {
+      return 40;
+    }
+    if (nameContains('gold')) {
+      return 45;
+    }
+    return 0;
+  };
+  setViewport = function(wdt) {
+    return $('meta[name=viewport]').attr('content', "width=" + wdt);
+  };
+  $.getJSON('http://tools.michaelzinn.de/mc/shopadmin/price_json.php?callback=?', function(data) {
+    var center, div, divs, e, item, items, min, prices, s, sizes, type, wdt, winwdt, _i, _j, _len, _len2;
+    wdt = 0;
+    divs = $();
+    for (type in data) {
+      items = data[type];
+      items.sort(function(x, y) {
+        return compare(items, x, y);
+      });
+      for (_i = 0, _len = items.length; _i < _len; _i++) {
+        item = items[_i];
+        div = generatePriceInfoDiv(item);
+        prices = div.children('.priceL, .priceR');
+        divs = divs.add(prices);
+        $("#" + type).append(div);
+        for (_j = 0, _len2 = prices.length; _j < _len2; _j++) {
+          e = prices[_j];
+          e = $(e);
+          if (e.width() > wdt) {
+            wdt = e.width();
+          }
+        }
+      }
+    }
+    divs.width(wdt);
+    $(document).trigger('itemsloaded');
+    sizes = function() {
+      var container, cwdt, itemwdt;
+      container = $('#blocks, #items');
+      itemwdt = $('.product', container).outerWidth(true);
+      cwdt = container.width();
+      return {
+        container: container,
+        itemwdt: itemwdt,
+        cwdt: cwdt,
+        row: Math.floor(cwdt / itemwdt)
+      };
+    };
+    center = function() {
+      var s;
+      s = sizes();
+      return s.container.css('padding-left', (s.cwdt - s.row * s.itemwdt) / 2);
+    };
+    center();
+    $(window).resize(center);
+    winwdt = $(window).width();
+    if (winwdt < 800) {
+      s = sizes();
+      if (s.row < 3) {
+        min = 0;
+        if (winwdt < 300) {
+          min = 2 * s.itemwdt;
+        } else {
+          min = 3 * s.itemwdt;
+        }
+        $('body').css('min-width', min);
+        s.container.css('padding-left', 0);
+        setViewport(min);
+      }
+    }
+    return null;
+  });
+}).call(this);
---
+++ search.js
@@ -0,0 +1,75 @@
+(function() {
+  var Item, cache, clearButton, clearSearch, fadeButton, form, input, unfadeButton;
+  clearButton = '<div class="icon16" id="clearButton">\n	<img src="Icons/cross-button-icon.png" alt="Clear" title="Sucheingabe l&ouml;schen">\n</div>';
+  form = '<form id=search>\n	<input type=text>\n</form>';
+  $('#searchfield').append(clearButton);
+  $('#searchfield').append(form);
+  form = $('#search');
+  clearButton = $('#clearButton');
+  clearButton.addClass('faded');
+  input = $('input', form);
+  cache = [];
+  Item = (function() {
+    function Item(img) {
+      this.title = img.attr('title');
+      this.element = img.parent().parent();
+    }
+    Item.prototype.highlight = function(flag) {
+      if (flag) {
+        return this.element.removeClass('nohighlight').addClass('highlight');
+      } else {
+        return this.element.removeClass('highlight').addClass('nohighlight');
+      }
+    };
+    Item.prototype.test = function(str) {
+      if (str.length === 0) {
+        return this.element.removeClass('nohighlight').removeClass('highlight');
+      } else {
+        return this.highlight(this.title.toLowerCase().indexOf(str.toLowerCase()) !== -1);
+      }
+    };
+    return Item;
+  })();
+  $(document).bind('itemsloaded', function() {
+    return $('.product img').not('#example img').each(function() {
+      return cache.push(new Item($(this)));
+    });
+  });
+  clearSearch = function() {
+    input.val('');
+    return fadeButton();
+  };
+  fadeButton = function() {
+    return clearButton.addClass('faded').removeClass('notfaded');
+  };
+  unfadeButton = function() {
+    return clearButton.addClass('notfaded').removeClass('faded');
+  };
+  form.submit(function(event) {
+    return event.preventDefault();
+  });
+  clearButton.click(function() {
+    clearSearch();
+    return input.keyup();
+  });
+  input.keydown(function(event) {
+    if (event.keyCode === 27) {
+      clearSearch();
+      return event.preventDefault();
+    }
+  });
+  input.keyup(function() {
+    var item, needle, _i, _len;
+    needle = input.val();
+    if (needle === '') {
+      fadeButton();
+    } else {
+      unfadeButton();
+    }
+    for (_i = 0, _len = cache.length; _i < _len; _i++) {
+      item = cache[_i];
+      item.test(needle);
+    }
+    return null;
+  });
+}).call(this);
